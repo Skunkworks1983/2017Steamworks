f97bc253074646a42e756544d3e0a4a98258966e
bronte999 <isabella.rahs@gmail.com>
2017-01-23 19:04:51 -0800

    Revised code from Code Review

--- src/Subsystems/cMotor.cpp
+++ src/Subsystems/cMotor.cpp
@@ -9,42 +9,51 @@
 #include <CANTalon.h>
 #include <RobotMap.h>
 
-cMotor::cMotor(int port):m_motor(port) {
-	// TODO Auto-generated constructor stub
+cMotor::cMotor(int port) :
+        m_motor(port)
+{
 }
 
-cMotor::~cMotor() {
-	// TODO Auto-generated destructor stub
+cMotor::~cMotor()
+{
 }
- bool cMotor::setBrakeMode(bool brake) {
-	if(brake == true){
-		 m_motor.ConfigNeutralMode(CANSpeedController::kNeutralMode_Brake);
-		 return true;
-	}
-	else{
-		m_motor.ConfigNeutralMode(CANSpeedController::kNeutralMode_Coast);
-		return false;
-	}
+void cMotor::setBrakeMode(bool brake)
+
+{
+    if(brake == true)
+    {
+        m_motor.ConfigNeutralMode(CANSpeedController::kNeutralMode_Brake);
+    }
+    else
+    {
+        m_motor.ConfigNeutralMode(CANSpeedController::kNeutralMode_Coast);
+    }
 
 }
-void cMotor::setOutput(float output) {
-	m_motor.Set(output);
+void cMotor::setOutput(float output)
+{
+    m_motor.Set(output);
 }
-void cMotor::PIDWrite(float output){
-	m_motor.Set(output);
+void cMotor::PIDWrite(float output)
+{
+    m_motor.Set(output);
 }
-double cMotor::PIDGet(){
-	return m_motor.GetEncPosition();
-	//we need to change this, not right
-	//return sensor output for pid
+double cMotor::PIDGet()
+{
+    return m_motor.GetEncPosition();
+    //we need to change this, not right
+    //return sensor output for pid
 }
 
-void cMotor::setEnabled(bool enabled){
-	if (m_motor.IsEnabled() && !enabled) {
-		m_motor.Enable();
-	}
-	if (!m_motor.IsEnabled() && enabled) {
-		m_motor.Disable();
-	}
+void cMotor::setEnabled(bool enabled)
+{
+    if(m_motor.IsEnabled() && !enabled)
+    {
+        m_motor.Enable();
+    }
+    if(!m_motor.IsEnabled() && enabled)
+    {
+        m_motor.Disable();
+    }
 
 }
--- src/Subsystems/cMotor.h
+++ src/Subsystems/cMotor.h
@@ -8,16 +8,17 @@
 #ifndef SRC_SUBSYSTEMS_CMOTOR_H_
 #define SRC_SUBSYSTEMS_CMOTOR_H_
 #include <CANTalon.h>
-class cMotor: public PIDSource, public PIDOutput {
+class cMotor: public PIDSource, public PIDOutput
+{
 private:
-	CANTalon m_motor;
+    CANTalon m_motor;
 public:
-	cMotor(int port);
-	virtual ~cMotor();
-	bool setBrakeMode(bool brake);
-	void setOutput(float output);
-	void PIDWrite(float output) override;
-	double PIDGet() override;
-	void setEnabled(bool enabled);
+    cMotor(int port);
+    virtual ~cMotor();
+    void setBrakeMode(bool brake);
+    void setOutput(float output);
+    void PIDWrite(float output) override;
+    double PIDGet() override;
+    void setEnabled(bool enabled);
 };
 #endif /* SRC_SUBSYSTEMS_CMOTOR_H_ */
