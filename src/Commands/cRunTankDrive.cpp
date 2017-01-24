6dc1c699f1da0daa620ce1bfe485be19a2134edf
bronte999 <isabella.rahs@gmail.com>
2017-01-23 18:55:21 -0800

    Revisions made on Code Review

--- src/Commands/cRunTankDrive.cpp
+++ src/Commands/cRunTankDrive.cpp
@@ -1,37 +1,36 @@
-/*
- * runTankDrive.cpp
- *
- *  Created on: Jan 16, 2017
- *      Author: s-4036956
- */
-//^^^ fix auto generated comments
 #include <Commands/cRunTankDrive.h>
 #include <Subsystems/cDriveBase.h>
 #include <OI.h>
 #include <CommandBase.h>
-cRunTankDrive::cRunTankDrive() {
+cRunTankDrive::cRunTankDrive()
+{
 
-	Requires(s_drivebase);
+    Requires(s_drivebase);
 }
 
-void cRunTankDrive::Initialize(){
-	CommandBase::s_drivebase->setEnabled(true);
+void cRunTankDrive::Initialize()
+{
+    CommandBase::s_drivebase->setEnabled(true);
 }
 
-void cRunTankDrive::Execute(){
-	CommandBase::s_drivebase->setLeftSpeed(CommandBase::s_oi->getLeftStickY());
-	CommandBase::s_drivebase->setRightSpeed(CommandBase::s_oi->getRightStickY());
+void cRunTankDrive::Execute()
+{
+    CommandBase::s_drivebase->setLeftSpeed(CommandBase::s_oi->getLeftStickY());
+    CommandBase::s_drivebase->setRightSpeed(CommandBase::s_oi->getRightStickY());
 }
 
-bool cRunTankDrive::IsFinished(){
-	return false;
+bool cRunTankDrive::IsFinished()
+{
+    return false;
 }
 
-void cRunTankDrive::End(){
+void cRunTankDrive::End()
+{
 
 }
 
-void cRunTankDrive::Interrupted(){
-	End();
+void cRunTankDrive::Interrupted()
+{
+    End();
 }
 
--- src/Commands/cRunTankDrive.h
+++ src/Commands/cRunTankDrive.h
@@ -9,18 +9,17 @@
 #define SRC_COMMANDS_CRUNTANKDRIVE_H_
 #include <CommandBase.h>
 #include <cstdbool>
-//program initiates tank drive. Makes robot move when joystick move in a w ay that resembles a tank so cool
-class cRunTankDrive: public CommandBase {
+class cRunTankDrive: public CommandBase
+{
 private:
-	bool driveState = true; //m_
+    bool driveState = true; //m_
 
 public:
-	cRunTankDrive();
-	void Initialize();
-	void Execute();
-	bool IsFinished();
-	void End();
-	void Interrupted();
+    cRunTankDrive();
+    void Initialize();
+    void Execute();bool IsFinished();
+    void End();
+    void Interrupted();
 };
 
 #endif /* SRC_COMMANDS_CRUNTANKDRIVE_H_ */
