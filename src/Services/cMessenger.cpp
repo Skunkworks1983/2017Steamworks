8b111eaaa9b989dd6c3533146472ad2526dd6645
Nathan Tresham <n473r5@gmail.com>
2017-02-08 20:25:55 -0800

    fixed messenger formatting based on review

--- src/Services/cMessenger.cpp
+++ src/Services/cMessenger.cpp
@@ -16,70 +16,37 @@
 // Creates a new messenger instance
 cMessenger::cMessenger(const char *server, const char *port)
 {
-    socklen_t addrlen = sizeof(m_remaddr); /* length of addresses */
-    int recvlen; /* # bytes received */
-    unsigned char buf[MSG_LEN]; /* receive buffer */
-
-    /* create a UDP socket */
-
+    // create a new socket
     if((m_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
-        perror("cannot create socket\n");
+        std::cout << "failed to create socket, errno " << errno << "\n";
         return;
     }
 
-    /* bind the socket to any valid IP address and a specific port */
-
+    // configure the settings for our own address configuration (us)
     memset((char *) &m_myaddr, 0, sizeof(m_myaddr));
     m_myaddr.sin_family = AF_INET;
     m_myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    m_myaddr.sin_port = htons(8888);
+    m_myaddr.sin_port = htons((int) port);
 
+    // configure the settings for the target we're sending to (raspberry pies)
     memset((char *) &m_remaddr, 0, sizeof(m_remaddr));
     m_remaddr.sin_family = AF_INET;
-    inet_pton(AF_INET, "10.19.83.100", &(m_remaddr.sin_addr));
-    m_remaddr.sin_port = htons(8888);
+    inet_pton(AF_INET, server, &(m_remaddr.sin_addr));
+    m_remaddr.sin_port = htons((int) port);
 
-    if(bind(m_sock, (struct sockaddr *) &m_myaddr, sizeof(m_myaddr)) < 0)
+    // bind to the socket using the configuration of our own host, as defined above
+    if(bind(m_sock, (struct sockaddr*) &m_myaddr, sizeof(m_myaddr)) < 0)
     {
-        perror("bind failed");
+        std::cout << "failed to bind to socket, errno " << errno << "\n";
         return;
     }
-
-    /*
-     m_isPostMatch = false;
-
-     struct addrinfo hints;
-     struct sockaddr_in myaddr;
-     int addrinfo_stat;
-
-     // Create the out variable for the connection settings
-     memset((char*) &hints, 0, sizeof(hints));
-     hints.ai_socktype = SOCK_DGRAM;
-     hints.ai_family = AF_INET;
-     hints.ai_flags = AI_NUMERICSERV;
-     hints.ai_protocol = 0;
-
-     // Attempt to get the address information of the system we're connecting to
-     // for later, and save the settings in hints & info
-     if((addrinfo_stat = getaddrinfo(RPI_IP, RPI_PORT, &hints, &m_info)) != 0)
-     {
-     std::cout << "couldn't get address info, error: " << errno << "\n";
-     return;
-     }
-
-     // Attempt to create the socket
-     if((m_sock = socket(m_info->ai_family, m_info->ai_socktype, m_info->ai_protocol)) == -1)
-     {
-     std::cout << "couldn't create socket, error: " << errno << "\n";
-     return;
-     }
-     */
 }
 
 cMessenger::~cMessenger()
 {
-
+    free(&m_myaddr);
+    free(&m_remaddr);
 }
 
 // Sends a string through the socket
