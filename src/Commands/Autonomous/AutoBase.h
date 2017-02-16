ac6d1bc8db6bfec11f5fdbefd26a40650b243c7f
Nathan Tresham <n473r5@gmail.com>
2017-02-15 20:32:41 -0800

    added headers from code review results :>

--- src/Commands/Autonomous/AutoBase.h
+++ src/Commands/Autonomous/AutoBase.h
@@ -3,13 +3,29 @@
  *
  *  Created on: Jan 26, 2017
  *      Author: s-4036956
+ *
+ * This class is to create command groups for
+ * the autonomous period. The AutoBase itself is
+ * a command group, and calling member functions such
+ * as goLift1 add groups of commands to the new
+ * AutoBase command group, which then becomes the
+ * entire list of commands for everything in the autonomous period.
+ *
+ * Each of the functions that return a new AutoBase
+ * work by creating their own AutoBase (essentially a CommandGroup), and
+ * adding it to the current master list of commands for
+ * the autonomous period.
+ *
+ * tl;dr: we add commandgroups for small auto functions (go to lift 1),
+ * and add them to the master list of auto commands (go to lift 1, then
+ * place gear on lift 1).
  */
 
 #ifndef SRC_COMMANDS_AUTONOMOUS_AUTOBASE_H_
 #define SRC_COMMANDS_AUTONOMOUS_AUTOBASE_H_
 #include <Commands/CommandGroup.h>
 
-class AutoBase: CommandGroup
+class AutoBase: public CommandGroup
 {
 private:
 
