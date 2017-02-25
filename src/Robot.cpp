4d4965a25f5abc0444ade6914ea1c73fe60afc3e
Unknown <benevedeselias@gmail.com>
2017-02-25 13:29:07 -0800

    Added back messenger

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -47,8 +47,8 @@ private:
 
         CommandBase::s_oi = new OI();
 
-        /*CommandBase::s_boilerMessenger = new cMessenger(BOILER_PI_IP, BOILER_PI_PORT);
-        CommandBase::s_liftMessenger = new cMessenger(GEAR_PI_IP, GEAR_PI_PORT);*/
+        CommandBase::s_boilerMessenger = new cMessenger(BOILER_PI_IP, BOILER_PI_PORT);
+        CommandBase::s_liftMessenger = new cMessenger(GEAR_PI_IP, GEAR_PI_PORT);
 
         //CommandBase::s_drivebase->getGyro()->initGyro();
         //CommandBase::s_drivebase->getGyro()->zeroYaw();*/
@@ -89,8 +89,8 @@ private:
     {
         Scheduler::GetInstance()->Run();
 
-        /*CommandBase::s_boilerMessenger->sendMessage("auto");
-        CommandBase::s_liftMessenger->sendMessage("auto");*/
+        CommandBase::s_boilerMessenger->sendMessage("auto");
+        CommandBase::s_liftMessenger->sendMessage("auto");
     }
 
     void TeleopInit()
@@ -106,8 +106,8 @@ private:
     {
         Scheduler::GetInstance()->Run();
 
-        //CommandBase::s_boilerMessenger->sendMessage("tele");
-        //CommandBase::s_liftMessenger->sendMessage("tele");
+        CommandBase::s_boilerMessenger->sendMessage("tele");
+        CommandBase::s_liftMessenger->sendMessage("tele");
     }
 
     void TestPeriodic()
@@ -125,7 +125,7 @@ private:
     	dashboard->PutNumber("Color sensor B value", CommandBase::s_drivebase->GetBValue());
     	dashboard->PutNumber("Color sensor C value", CommandBase::s_drivebase->GetCValue());*/
 
-    	//dashboard->PutNumber("Camera: Gear tape x pos", CommandBase::s_liftMessenger->receiveLiftData()->getX());
+    	SmartDashboard::PutNumber("Camera: Gear tape x pos", CommandBase::s_liftMessenger->receiveLiftData()->getX());
 
         SmartDashboard::PutNumber("Drivebase: left encoder", CommandBase::s_drivebase->getMotorGroupLeft()->getPosition());
         SmartDashboard::PutNumber("Drivebase: right encoder", CommandBase::s_drivebase->getMotorGroupRight()->getPosition());
