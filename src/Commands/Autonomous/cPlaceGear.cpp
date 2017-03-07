17852ee1be1734462c54ec127d8c8fdde9c63db7
linknmike <mikeheidal@gmail.com>
2017-03-06 19:26:49 -0800

    cPlaceGear takes positioning into account

--- src/Commands/Autonomous/cPlaceGear.cpp
+++ src/Commands/Autonomous/cPlaceGear.cpp
@@ -4,39 +4,38 @@
 #include <Commands/Autonomous/cCenterTape.h>
 #include <Commands/Autonomous/cGearPath.h>
 
-AutoBase* AutoBase::placeGear()
-{
-    AutoBase* placeGear = new AutoBase();
-    m_placeGear = new CommandGroup;
-    switch (startPosition) {
+AutoBase* AutoBase::placeGear() {
+	AutoBase* placeGear = new AutoBase();
+	m_placeGear = new CommandGroup;
+	switch (startPosition) {
 
-    case POS_BOILER:
- 	   m_placeGear->AddSequential(new cSimpleDriveForward(10, true));
- 	   m_placeGear->AddSequential(new cCenterTape());
- 	   m_placeGear->AddSequential(new cGearPath());
- 	   m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleTapeRobotPivotPoint));
- 	   m_placeGear->AddSequential(new cSimpleDriveForward(AutoBase::s_distanceToPivotPoint));
- 	   m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleRobotPivotPointPeg));
- 	   m_placeGear->AddSequential(new cSimpleDriveForward(DISTANCE_FROM_PEG_TO_PIVOT_POINT));
- 	   break;
-    case POS_CENTER:
-       m_placeGear->AddSequential(new cCenterTape());
-       m_placeGear->AddSequential(new cGearPath());
-       m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleTapeRobotPivotPoint));
-       m_placeGear->AddSequential(new cSimpleDriveForward(AutoBase::s_distanceToPivotPoint));
-       m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleRobotPivotPointPeg));
-       m_placeGear->AddSequential(new cSimpleDriveForward(DISTANCE_FROM_PEG_TO_PIVOT_POINT));
-       break;
-    case POS_RETRIEVAL:
-    	m_placeGear->AddSequential(new cSimpleDriveForward(10, true));
-    	m_placeGear->AddSequential(new cCenterTape());
-    	m_placeGear->AddSequential(new cGearPath());
-    	m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleTapeRobotPivotPoint));
-    	m_placeGear->AddSequential(new cSimpleDriveForward(AutoBase::s_distanceToPivotPoint));
-    	m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleRobotPivotPointPeg));
-    	m_placeGear->AddSequential(new cSimpleDriveForward(DISTANCE_FROM_PEG_TO_PIVOT_POINT));
-    	break;
-       }
+	case POS_BOILER:
+		m_placeGear->AddSequential(new cSimpleDriveForward(10, true));
+		m_placeGear->AddSequential(new cCenterTape());
+		m_placeGear->AddSequential(new cGearPath());
+		m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleTapeRobotPivotPoint));
+		m_placeGear->AddSequential(new cSimpleDriveForward(AutoBase::s_distanceToPivotPoint));
+		m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleRobotPivotPointPeg));
+		m_placeGear->AddSequential(new cSimpleDriveForward(DISTANCE_FROM_PEG_TO_PIVOT_POINT));
+		break;
+	case POS_CENTER:
+		m_placeGear->AddSequential(new cCenterTape());
+		m_placeGear->AddSequential(new cGearPath());
+		m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleTapeRobotPivotPoint));
+		m_placeGear->AddSequential(new cSimpleDriveForward(AutoBase::s_distanceToPivotPoint));
+		m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleRobotPivotPointPeg));
+		m_placeGear->AddSequential(new cSimpleDriveForward(DISTANCE_FROM_PEG_TO_PIVOT_POINT));
+		break;
+	case POS_RETRIEVAL:
+		m_placeGear->AddSequential(new cSimpleDriveForward(10, true));
+		m_placeGear->AddSequential(new cCenterTape());
+		m_placeGear->AddSequential(new cGearPath());
+		m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleTapeRobotPivotPoint));
+		m_placeGear->AddSequential(new cSimpleDriveForward(AutoBase::s_distanceToPivotPoint));
+		m_placeGear->AddSequential(new cTurnDegree(AutoBase::s_angleRobotPivotPointPeg));
+		m_placeGear->AddSequential(new cSimpleDriveForward(DISTANCE_FROM_PEG_TO_PIVOT_POINT));
+		break;
+	}
 
-    return placeGear;
+	return placeGear;
 }
